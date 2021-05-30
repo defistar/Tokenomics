5a0549b18c3a78324ec2954b4e9288323fae5008
lakshmikanth p <lakshmikanthp@lakshmikanths-MacBook-Pro.local>
2021-05-30 22:26:51 +0800

    fix imports in token contract

--- contracts/kanthDeFiToken.sol
+++ contracts/kanthDeFiToken.sol
@@ -3,6 +3,8 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 
 contract KanthDeFiToken is AccessControl, ERC20, ERC20Pausable {
     
